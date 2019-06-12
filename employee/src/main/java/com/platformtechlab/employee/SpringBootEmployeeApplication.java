16cbda286218ce003ed07ad8df22b25d06c58a68
Cholhong Im <imich@naver.com>
2019-06-12 18:27:02 +0900

    employee config update

--- employee/src/main/java/com/platformtechlab/employee/SpringBootEmployeeApplication.java
+++ employee/src/main/java/com/platformtechlab/employee/SpringBootEmployeeApplication.java
@@ -2,7 +2,9 @@ package com.platformtechlab.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient 
 @SpringBootApplication
 public class SpringBootEmployeeApplication {
 	 
