eed52353738ecf2e5f65c0c12b223b1a0f426dff
Cholhong Im <imich@naver.com>
2019-06-24 14:49:04 +0900

    swagger

--- employee/src/main/java/com/platformtechlab/employee/SwaggerConfig.java
+++ employee/src/main/java/com/platformtechlab/employee/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.platformtechlab.employee;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.base.Predicates;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -12,13 +14,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any()) // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.cloud")))
                 .paths(PathSelectors.ant("/**")) // 그중 /** 인 URL들만 필터링
+              //.paths(PathSelectors.regex("/api/.*"))
+                .apis(RequestHandlerSelectors.any()) // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
                 .build();
     }
 }
