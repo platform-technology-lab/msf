1a7ffc27919d616959db111b4ad99bd67e5d6260
Cholhong Im <imich@naver.com>
2019-10-04 19:18:25 +0900

    controller url fix

--- clientexample/src/main/java/com/platformtechlab/msf/clientExample/ClientExampleController.java
+++ clientexample/src/main/java/com/platformtechlab/msf/clientExample/ClientExampleController.java
@@ -20,7 +20,7 @@ public class ClientExampleController {
 
     @RequestMapping(value="/emplist", method=RequestMethod.GET)
     public String listEmployee(Model model) {
-    	Employee[] employees = employeeClient.getForObject("http://localhost:8012/employees",Employee[].class);
+    	Employee[] employees = employeeClient.getForObject("http://basic:8012/employees",Employee[].class);
     	uiData.setEmployees(Arrays.asList(employees));
        model.addAttribute("uidata",uiData );
         return "list";
@@ -28,7 +28,7 @@ public class ClientExampleController {
 
     @RequestMapping(value="/empinfo/{empNo}", method=RequestMethod.GET)
     public String Infoemployee(Model model, @PathVariable("empNo") String empNo) {
-    	Employee employee = employeeClient.getForObject("http://localhost:8012/employee/"+empNo, Employee.class);
+    	Employee employee = employeeClient.getForObject("http://basic:8012/employee/"+empNo, Employee.class);
     	uiData.setEmployee(employee);
        model.addAttribute("uidata",uiData );
         return "employee";
@@ -37,7 +37,7 @@ public class ClientExampleController {
     @RequestMapping(value="/addemployee/{action}/{empNo}", method=RequestMethod.GET)
     public String addEmployeeForm(Model model, @PathVariable("action") String action, @PathVariable("empNo") String empNo) {
     	if (action.equals("UPDATE") && !empNo.contentEquals("new")) {
-    		Employee employee = employeeClient.getForObject("http://localhost:8012/employee/"+empNo, Employee.class);
+    		Employee employee = employeeClient.getForObject("http://basic:8012/employee/"+empNo, Employee.class);
         	uiData.setEmployee(employee);
         	uiData.setAction("UPDATE");
     	} else uiData.setAction("ADD");
@@ -48,15 +48,15 @@ public class ClientExampleController {
     @RequestMapping(value="/addemployee/{action}", method=RequestMethod.POST)
     public String addEmployee(@ModelAttribute UIData uiData, Model model, @PathVariable("action") String action) throws RestClientException, UnsupportedEncodingException {
     	if (action.equals("ADD"))
-    	 employeeClient.postForObject("http://localhost:8012/employee", uiData.getEmployee(), Employee.class);
+    	 employeeClient.postForObject("http://basic:8012/employee", uiData.getEmployee(), Employee.class);
     	else
-    	 employeeClient.put("http://localhost:8012/employee", uiData.getEmployee());	
+    	 employeeClient.put("http://basic:8012/employee", uiData.getEmployee());	
         return "redirect:/emplist"; 
     }
     
     @RequestMapping(value="/delemployee/{empNo}", method=RequestMethod.GET)
     public String delEmployee(Model model,@PathVariable("empNo") String empNo) {
-    	employeeClient.delete("http://localhost:8012/employee/"+empNo); 
+    	employeeClient.delete("http://basic:8012/employee/"+empNo); 
         return "redirect:/emplist";
     }
 
